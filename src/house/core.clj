22853b669fe3134466dd6a30a3a88f7357c39b00
Nikhil Prabhakar <nprabhak@nprabhak-mn.local>
2013-12-02 15:22:52 +0530

    First commit.

---
+++ src/house/core.clj
@@ -0,0 +1,8 @@
+(ns house.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(+ 1 2)
---
+++ src/house/scratch.clj
@@ -0,0 +1,33 @@
+(ns house.scratch
+  (:use [cascalog.api])
+  (:use [cascalog.playground])
+  (:use [cascalog.more-taps :only (hfs-delimited)])
+  (:gen-class))
+
+(defn hello-world
+  []
+  (println "Hello world!"))
+
+(comment
+  (defn -main [in out & args]
+    (?<- (hfs-delimited out)
+         [?doc ?line]
+         ((hfs-delimited in :skip-header? true) ?doc ?line)))
+  
+  (?<- (stdout)
+       [?doc ?line]
+       ((hfs-delimited "/Users/nprabhak/Softwares/Hadoop/Cascalog/Impatient/part1/data/rain.txt" :skip-header? true) ?doc ?line))
+
+  (?<- (hfs-delimited "/tmp/out")
+       [?doc ?line]
+       ((hfs-delimited "/Users/nprabhak/Softwares/Hadoop/Cascalog/Impatient/part1/data/rain.txt" :skip-header? true) ?doc ?line))
+
+  "(nprabhak@nprabhak-mn ~/Projects/Clojure/house)$ hadoop jar target/house-0.1.0-SNAPSHOT-standalone.jar house.scratch /data/rain.txt /cascalog-out"
+
+  (?<- (hfs-delimited "hdfs://localhost:9000/cascalog-out")
+       [?doc ?line]
+       ((hfs-delimited "hdfs://localhost:9000/data/rain.txt" :skip-header? true) ?doc ?line)))
+
+(comment
+  (?- (stdout)
+      sentence))
